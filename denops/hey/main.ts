8006bed21c8d519952879fb0e1f54c37a0219e5a
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-02-07 14:48:05 +0000

    Add Hey! command

--- denops/hey/main.ts
+++ denops/hey/main.ts
@@ -1,14 +1,14 @@
-import { ChatOpenAI } from "https://esm.sh/langchain@0.0.68/chat_models/openai";
-import { HumanChatMessage, SystemChatMessage } from "https://esm.sh/langchain@0.0.68/schema";
+import { ChatOpenAI } from "https://esm.sh/langchain@0.1.14/chat_models/openai";
+import { HumanChatMessage, SystemChatMessage } from "https://esm.sh/langchain@0.1.14/schema";
 import { Mutex } from "https://lib.deno.dev/x/async@v2/mod.ts";
-import { Denops } from "https://lib.deno.dev/x/denops_std@v4/mod.ts";
-import * as helper from "https://lib.deno.dev/x/denops_std@v4/helper/mod.ts";
-import * as vars from "https://lib.deno.dev/x/denops_std@v4/variable/mod.ts";
-import * as option from "https://lib.deno.dev/x/denops_std@v4/option/mod.ts";
-import * as fn from "https://lib.deno.dev/x/denops_std@v4/function/mod.ts";
-import * as buffer from "https://lib.deno.dev/x/denops_std@v4/buffer/mod.ts";
+import { Denops } from "https://lib.deno.dev/x/denops_std@v6/mod.ts";
+import * as helper from "https://lib.deno.dev/x/denops_std@v6/helper/mod.ts";
+import * as vars from "https://lib.deno.dev/x/denops_std@v6/variable/mod.ts";
+import * as option from "https://lib.deno.dev/x/denops_std@v6/option/mod.ts";
+import * as fn from "https://lib.deno.dev/x/denops_std@v6/function/mod.ts";
+import * as buffer from "https://lib.deno.dev/x/denops_std@v6/buffer/mod.ts";
 import outdent from "https://lib.deno.dev/x/outdent@v0.8.0/mod.ts";
-import * as popup from "https://deno.land/x/denops_popup@v2.2.0/mod.ts";
+import * as popup from "https://lib.deno.dev/x/denops_popup@v2/mod.ts";
 
 async function showPopup(denops: Denops): Promise<[number, number]> {
   const bufnr = await fn.bufnr(denops, "HeyVim", true);
@@ -58,6 +58,69 @@ async function showWindow(denops: Denops): Promise<[number, number]> {
 }
 
 /**
+ * The `hey$` function sends a message to the ChatOpenAI model registered with Denops.
+ * This function overwrites the original text with the response from the model.
+ *
+ * @param {Denops} denops - The Denops object for current buffer
+ * @param {number} firstline - The first line number of the range to send
+ * @param {number} lastline - The last line number of the range to send
+ * @param {string} request - The input text to send to the model
+ * @param {AbortController} controller - The AbortController to abort the request
+ * @returns {Promise<void>}
+ */
+async function hey$(denops: Denops, firstline: number, lastline: number, request: string, controller: AbortController) {
+  const precontext = (await fn.getline(denops, Math.max(firstline - 20, 0), firstline - 1)).join("\n");
+  const postcontext = (await fn.getline(denops, lastline + 1, lastline + 20)).join("\n");
+  const context = (await fn.getline(denops, firstline, lastline)).join("\n");
+  let bufnr: number = await fn.bufnr(denops, '.');
+  await fn.deletebufline(denops, bufnr, firstline, lastline);
+  await fn.appendbufline(denops, bufnr, firstline - 1, [""]);
+  let lastline2 = firstline;
+  const mutex = new Mutex();
+  const model = new ChatOpenAI({
+    openAIApiKey: await vars.g.get<string | undefined>(denops, "hey_openai_api_key", undefined),
+    modelName: await vars.g.get(denops, "hey_model_name", "gpt-3.5-turbo"),
+    verbose: await vars.g.get(denops, "hey_verbose", false),
+    streaming: true,
+    callbacks: [
+      {
+        async handleLLMNewToken(token: string) {
+          await mutex.acquire();
+          let lines = await fn.getbufline(denops, bufnr, lastline2);
+          lines = (lines.join("\n") + token).split("\n");
+          await fn.deletebufline(denops, bufnr, lastline2);
+          await fn.appendbufline(denops, bufnr, lastline2 - 1, lines);
+          await denops.cmd('redraw');
+          lastline2 += lines.length - 1;
+          mutex.release();
+        }
+      }
+    ]
+  });
+
+  const systemPrompt = outdent`
+    Act a professional ${ await vars.o.get(denops, "filetype") } writer for:
+    - helping human to write code (e.g., auto-completion)
+    - helping human to write prose (e.g., grammar/ spelling correction)
+
+    Write ${ await vars.o.get(denops, 'filetype') } code or prose.
+    Do not wrap the output with \`\`\`.
+  `;
+
+  const userPrompt = outdent`
+    <Prompt>${ request }</Prompt>
+    <PreContext>${ outdent.string("\n"+precontext) }</PreContext>
+    <Target>${ outdent.string("\n"+context) }</Target>
+    <PostContext>${ outdent.string("\n"+postcontext) }</PostContext>
+  `;
+
+  await model.call([
+    new SystemChatMessage(systemPrompt),
+    new HumanChatMessage(userPrompt)
+  ], { options: { signal: controller.signal }});
+}
+
+/**
  * The `hey` function sends a message to the ChatOpenAI model registered with Denops.
  *
  * @param {Denops} denops - The Denops object for current buffer
@@ -77,6 +140,7 @@ async function hey(denops: Denops, firstline: number, lastline: number, request:
   } else {
     bufnr = (await showPopup(denops))[0]
   }
+  let lastline2 = 1;
   const mutex = new Mutex();
   const model = new ChatOpenAI({
     openAIApiKey: await vars.g.get<string | undefined>(denops, "hey_openai_api_key", undefined),
@@ -87,10 +151,12 @@ async function hey(denops: Denops, firstline: number, lastline: number, request:
       {
         async handleLLMNewToken(token: string) {
           await mutex.acquire();
-          let lines = await fn.getbufline(denops, bufnr, 1, "$");
+          let lines = await fn.getbufline(denops, bufnr, lastline2);
           lines = (lines.join("\n") + token).split("\n")
-          await buffer.replace(denops, bufnr, lines);
+          await fn.deletebufline(denops, bufnr, lastline2);
+          await fn.appendbufline(denops, bufnr, lastline2 - 1, lines);
           await denops.cmd('redraw');
+          lastline2 += lines.length - 1;
           mutex.release();
         }
       }
@@ -125,13 +191,17 @@ export async function main(denops: Denops) {
   let controller: AbortController | undefined;
 
   denops.dispatcher = {
-    async hey(firstline: unknown, lastline: unknown, prompt: unknown) {
-      if (typeof(firstline) !== 'number' || typeof(lastline) !== 'number' || typeof(prompt) !== 'string') {
+    async hey(firstline: unknown, lastline: unknown, prompt: unknown, bang: unknown) {
+      if (typeof(firstline) !== 'number' || typeof(lastline) !== 'number' || typeof(prompt) !== 'string' || typeof(bang) !== 'string') {
         return
       }
       try {
         controller = new AbortController();
-        await hey(denops, firstline, lastline, prompt, controller);
+        if (bang === '!') {
+          await hey$(denops, firstline, lastline, prompt, controller);
+        } else {
+          await hey(denops, firstline, lastline, prompt, controller);
+        }
       } catch (e) {
         console.log(e);
       } finally {
@@ -144,10 +214,10 @@ export async function main(denops: Denops) {
     }
   };
   await helper.execute(denops, outdent`
-    function! Hey(prompt) range abort
-      call denops#notify("${denops.name}", "hey", [a:firstline, a:lastline, a:prompt])
+    function! Hey(prompt, bang) range abort
+      call denops#notify("${denops.name}", "hey", [a:firstline, a:lastline, a:prompt, a:bang])
     endfunction
-    command! -nargs=1 -range Hey <line1>,<line2>call Hey(<q-args>)
+    command! -bang -nargs=1 -range Hey <line1>,<line2>call Hey(<q-args>, '<bang>')
 
     function! HeyAbort() abort
       call denops#notify("${denops.name}", "abort", [])
