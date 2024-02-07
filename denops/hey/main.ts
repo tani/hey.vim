2793106b1cd26f5b4f8fc890114364fbefe28e4f
TANIGUCHI Masaya <ta2gch@gmail.com>
2024-02-07 20:10:11 +0000

    Merge hey and hey$

--- denops/hey/main.ts
+++ denops/hey/main.ts
@@ -2,16 +2,32 @@ import { ChatOpenAI } from "https://esm.sh/langchain@0.1.14/chat_models/openai";
 import { HumanChatMessage, SystemChatMessage } from "https://esm.sh/langchain@0.1.14/schema";
 import { Mutex } from "https://lib.deno.dev/x/async@v2/mod.ts";
 import { Denops } from "https://lib.deno.dev/x/denops_std@v6/mod.ts";
-import * as helper from "https://lib.deno.dev/x/denops_std@v6/helper/mod.ts";
-import * as vars from "https://lib.deno.dev/x/denops_std@v6/variable/mod.ts";
-import * as option from "https://lib.deno.dev/x/denops_std@v6/option/mod.ts";
-import * as fn from "https://lib.deno.dev/x/denops_std@v6/function/mod.ts";
 import * as buffer from "https://lib.deno.dev/x/denops_std@v6/buffer/mod.ts";
-import outdent from "https://lib.deno.dev/x/outdent@v0.8.0/mod.ts";
+import * as fn from "https://lib.deno.dev/x/denops_std@v6/function/mod.ts";
+import * as helper from "https://lib.deno.dev/x/denops_std@v6/helper/mod.ts";
+import * as option from "https://lib.deno.dev/x/denops_std@v6/option/mod.ts";
 import * as popup from "https://lib.deno.dev/x/denops_popup@v2/mod.ts";
+import * as vars from "https://lib.deno.dev/x/denops_std@v6/variable/mod.ts";
+import outdent from "https://lib.deno.dev/x/outdent@v0.8.0/mod.ts";
+import { is } from "https://lib.deno.dev/x/unknownutil@3/mod.ts";
 
-async function showPopup(denops: Denops): Promise<[number, number]> {
-  const bufnr = await fn.bufnr(denops, "HeyVim", true);
+/**
+ * shows a popup window with specific settings and dimensions based on the 
+ * current window size.
+ *
+ * this function creates a new buffer with markdown filetype and some predefined 
+ * settings like nofile, unlisted, and others to behave as a popup. it then 
+ * calculates the size and position of the popup window relative to the current 
+ * vim window dimensions to center the popup. finally, it opens the popup with 
+ * these calculated dimensions.
+ *
+ * @param {denops} denops - the denops instance, providing an interface to 
+ * interact with the neovim/vim editor.
+ * @returns {promise<[number, number]>} a promise that resolves to a tuple 
+ * containing the row and column where the popup is placed.
+ */
+async function showpopup(denops: denops): promise<[number, number]> {
+  const bufnr = await fn.bufnr(denops, "heyvim", true);
   await buffer.ensure(denops, bufnr, async () => {
     await option.buftype.set(denops, "nofile");
     await option.buflisted.set(denops, false);
@@ -25,10 +41,10 @@ async function showPopup(denops: Denops): Promise<[number, number]> {
   })
   const winh = await fn.winheight(denops, "%") as number;
   const winw = await fn.winwidth(denops, "%") as number;
-  const poph = Math.floor(winh * 0.8);
-  const popw = Math.floor(winw * 0.8);
-  const row = Math.floor((winh - poph) / 2);
-  const col = Math.floor((winw - popw) / 2);
+  const poph = math.floor(winh * 0.8);
+  const popw = math.floor(winw * 0.8);
+  const row = math.floor((winh - poph) / 2);
+  const col = math.floor((winw - popw) / 2);
   const winnr = await popup.open(denops, bufnr, {
     row, col, height: poph, width: popw, origin: "topleft", border: true,
   })
@@ -36,8 +52,16 @@ async function showPopup(denops: Denops): Promise<[number, number]> {
   return [bufnr, winnr]
 }
 
-async function showWindow(denops: Denops): Promise<[number, number]> {
-  const bufnr = await fn.bufnr(denops, "HeyVim", true);
+/**
+ * Opens a "heyvim" buffer in a new window within the Deno Vim/Neovim plugin.
+ * Closes the window if "heyvim" is already open, sets up the buffer without
+ * line numbers, swap files, and other standard Vim features for markdown
+ * presentation. Returns the buffer and window numbers.
+ * @param {denops} denops - Denops instance for Vim/Neovim API interaction.
+ * @returns {Promise<[number, number]>} Promise resolving to ["heyvim" buffer number, window number].
+ */
+async function showwindow(denops: denops): promise<[number, number]> {
+  const bufnr = await fn.bufnr(denops, "heyvim", true);
   const [winnr] = await fn.win_findbuf(denops, bufnr) as number[];
   if (winnr >= 0) {
     await fn.win_execute(denops, winnr, "close");
@@ -51,73 +75,10 @@ async function showWindow(denops: Denops): Promise<[number, number]> {
     await option.swapfile.set(denops, false);
     await option.wrap.set(denops, true);
     await option.filetype.set(denops, "markdown");
-    await denops.cmd(`vnew HeyVim`)
+    await denops.cmd(`vnew heyvim`)
     await fn.deletebufline(denops, bufnr, 1, "$");
   })
   return [bufnr, winnr]
-}
-
-/**
- * The `hey$` function sends a message to the ChatOpenAI model registered with Denops.
- * This function overwrites the original text with the response from the model.
- *
- * @param {Denops} denops - The Denops object for current buffer
- * @param {number} firstline - The first line number of the range to send
- * @param {number} lastline - The last line number of the range to send
- * @param {string} request - The input text to send to the model
- * @param {AbortController} controller - The AbortController to abort the request
- * @returns {Promise<void>}
- */
-async function hey$(denops: Denops, firstline: number, lastline: number, request: string, controller: AbortController) {
-  const precontext = (await fn.getline(denops, Math.max(firstline - 20, 0), firstline - 1)).join("\n");
-  const postcontext = (await fn.getline(denops, lastline + 1, lastline + 20)).join("\n");
-  const context = (await fn.getline(denops, firstline, lastline)).join("\n");
-  let bufnr: number = await fn.bufnr(denops, '.');
-  await fn.deletebufline(denops, bufnr, firstline, lastline);
-  await fn.appendbufline(denops, bufnr, firstline - 1, [""]);
-  let lastline2 = firstline;
-  const mutex = new Mutex();
-  const model = new ChatOpenAI({
-    openAIApiKey: await vars.g.get<string | undefined>(denops, "hey_openai_api_key", undefined),
-    modelName: await vars.g.get(denops, "hey_model_name", "gpt-3.5-turbo"),
-    verbose: await vars.g.get(denops, "hey_verbose", false),
-    streaming: true,
-    callbacks: [
-      {
-        async handleLLMNewToken(token: string) {
-          await mutex.acquire();
-          let lines = await fn.getbufline(denops, bufnr, lastline2);
-          lines = (lines.join("\n") + token).split("\n");
-          await fn.deletebufline(denops, bufnr, lastline2);
-          await fn.appendbufline(denops, bufnr, lastline2 - 1, lines);
-          await denops.cmd('redraw');
-          lastline2 += lines.length - 1;
-          mutex.release();
-        }
-      }
-    ]
-  });
-
-  const systemPrompt = outdent`
-    Act a professional ${ await vars.o.get(denops, "filetype") } writer for:
-    - helping human to write code (e.g., auto-completion)
-    - helping human to write prose (e.g., grammar/ spelling correction)
-
-    Write ${ await vars.o.get(denops, 'filetype') } code or prose.
-    Do not wrap the output with \`\`\`.
-  `;
-
-  const userPrompt = outdent`
-    <Prompt>${ request }</Prompt>
-    <PreContext>${ outdent.string("\n"+precontext) }</PreContext>
-    <Target>${ outdent.string("\n"+context) }</Target>
-    <PostContext>${ outdent.string("\n"+postcontext) }</PostContext>
-  `;
-
-  await model.call([
-    new SystemChatMessage(systemPrompt),
-    new HumanChatMessage(userPrompt)
-  ], { options: { signal: controller.signal }});
 }
 
 /**
@@ -128,19 +89,30 @@ async function hey$(denops: Denops, firstline: number, lastline: number, request
  * @param {number} lastline - The last line number of the range to send
  * @param {string} request - The input text to send to the model
  * @param {AbortController} controller - The AbortController to abort the request
+ * @param {string} bang - The bang to determine the behavior of the function
  * @returns {Promise<void>}
  */
-async function hey(denops: Denops, firstline: number, lastline: number, request: string, controller: AbortController) {
-  const precontext = (await fn.getline(denops, Math.max(firstline - 20, 0), firstline - 1)).join("\n");
-  const postcontext = (await fn.getline(denops, lastline + 1, lastline + 20)).join("\n");
-  const context = (await fn.getline(denops, firstline, lastline)).join("\n");
+async function hey(denops: Denops, firstline: number, lastline: number, request: string, controller: AbortController, bang: string = '') {
+  const precontext = (await fn.getline(denops, 0, firstline - 1)).join("\n").slice(-4000);
+  const postcontext = (await fn.getline(denops, lastline + 1, "$")).join("\n").slice(0, 4000);
+  const context = (await fn.getline(denops, firstline, lastline)).join("\n")
   let bufnr: number;
-  if (await vars.g.get(denops, "hey_message_style", "window") === "window") {
+  let lastline2: number;
+  let restriction = '';
+  if (bang === '!') {
+    bufnr = await fn.bufnr(denops, '.');
+    await fn.deletebufline(denops, bufnr, firstline, lastline);
+    await fn.appendbufline(denops, bufnr, firstline - 1, [""]);
+    lastline2 = firstline;
+    restriction += 'Do not wrap the output with the code block (e.g., ```python ... ```).\n';
+    restriction += 'Do not write the natural language text out of the code block.\n';
+  } else if (await vars.g.get(denops, "hey_message_style", "window") === "window") {
     bufnr = (await showWindow(denops))[0]
+    lastline2 = 1;
   } else {
     bufnr = (await showPopup(denops))[0]
+    lastline2 = 1;
   }
-  let lastline2 = 1;
   const mutex = new Mutex();
   const model = new ChatOpenAI({
     openAIApiKey: await vars.g.get<string | undefined>(denops, "hey_openai_api_key", undefined),
@@ -168,21 +140,25 @@ async function hey(denops: Denops, firstline: number, lastline: number, request:
     - helping human to write code (e.g., auto-completion)
     - helping human to write prose (e.g., grammar/ spelling correction)
 
-    The condition of the output is:
-    - markdown format.
-    - the concise text.
-    - no repetition of user prompt.
+    ${ restriction }
   `;
 
+  const precontextPromt = outdent`
+    [PreContext]${ outdent.string("\n"+precontext) }[/PreContext]
+  `
+  const postcontextPrompt = outdent`
+    [PostContext]${ outdent.string("\n"+postcontext) }[/PostContext]
+  `
+
   const userPrompt = outdent`
-    <Prompt>${ request }</Prompt>
-    <PreContext>${ outdent.string("\n"+precontext) }</PreContext>
-    <Target>${ outdent.string("\n"+context) }</Target>
-    <PostContext>${ outdent.string("\n"+postcontext) }</PostContext>
+    [Prompt]${ request }[/Prompt]
+    [Target]${ outdent.string("\n"+context) }[/Target]
   `;
 
   await model.call([
     new SystemChatMessage(systemPrompt),
+    new SystemChatMessage(precontextPromt),
+    new SystemChatMessage(postcontextPrompt),
     new HumanChatMessage(userPrompt)
   ], { options: { signal: controller.signal }});
 }
@@ -192,16 +168,14 @@ export async function main(denops: Denops) {
 
   denops.dispatcher = {
     async hey(firstline: unknown, lastline: unknown, prompt: unknown, bang: unknown) {
-      if (typeof(firstline) !== 'number' || typeof(lastline) !== 'number' || typeof(prompt) !== 'string' || typeof(bang) !== 'string') {
+      const isArguments = is.TupleOf([is.Number, is.Number, is.String, is.String]);
+      if (!isArguments([firstline, lastline, prompt, bang])) {
+        console.warn('Invalid arguments');
         return
       }
       try {
         controller = new AbortController();
-        if (bang === '!') {
-          await hey$(denops, firstline, lastline, prompt, controller);
-        } else {
-          await hey(denops, firstline, lastline, prompt, controller);
-        }
+        await hey(denops, firstline, lastline, prompt, controller, bang);
       } catch (e) {
         console.log(e);
       } finally {
